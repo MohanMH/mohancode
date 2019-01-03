5013dd48a11ef7c638b157d4c958b4ad75af340d
MohanMH <43338578+MohanMH@users.noreply.github.com>
2019-01-03 14:31:23 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 void main()
 {
-int n,n1,temp=0,c;
+int n,a,temp=0,c;
 printf("enter the number");
 scanf("%d",&n);
 c=n;
 while(n>0)
 {
-n1=n%10;
-temp=temp+(n1*n1*n1);
+a=n%10;
+temp=temp+(a*a*a);
 n=n/10;
 }
 printf("%d",temp);
