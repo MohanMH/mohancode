4fea7f95332ddef62fa71175f9f5900cf8718942
MohanMH <43338578+MohanMH@users.noreply.github.com>
2019-01-03 14:43:15 +0530

    Create Nnumbers.c

---
+++ Nnumbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a=1;
+    int i,n,sum;
+    scanf("%d",&n);
+    for(i=1;i<6;i++)
+    {   sum=n*i;
+        printf("%d*%d=%d",n,i,sum);
+        printf("\n");
+    }
+}
