9d200abc05b27c6df0d1d581dc5ecd01ee23bcda
MohanMH <43338578+MohanMH@users.noreply.github.com>
2019-01-29 11:38:38 +0530

    Create ArrayN.c

---
+++ ArrayN.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,i,gcd;
+	scanf("%d %d",&a,&b);
+	
+	for(i=2;i<=a&&i<=b;i++)
+	{
+              if(a%i==0&&b%i==0)
+              gcd=i;
+	}
+	printf("%d %d",gcd,gcd);
+	return 0;
+}
